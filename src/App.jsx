56c15576f74528b1c9ddf8d0aa4cedec16e2b6f3
Olaitan Feranmi <olaitanferanmi41@gmail.com>
2023-07-27 16:03:16 -0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import Geolocation from './components/Geolocation';
+
 import Quote from './components/quote';
 
 function App() {
